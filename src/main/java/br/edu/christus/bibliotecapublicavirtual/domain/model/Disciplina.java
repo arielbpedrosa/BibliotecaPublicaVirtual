13fea05b160a1d824554aed43222d93c0355079e
Joao Victor <jvnhonorato@gmail.com>
2026-05-06 01:26:46 -0300

    classes Enums criadas

---
+++ src/main/java/br/edu/christus/bibliotecapublicavirtual/domain/model/Disciplina.java
@@ -0,0 +1,13 @@
+package br.edu.christus.bibliotecapublicavirtual.domain.model;
+
+public enum Disciplina {
+    MATEMATICA,
+    PORTUGUES,
+    HISTORIA,
+    GEOGRAFIA,
+    FISICA,
+    QUIMICA,
+    BIOLOGIA,
+    INGLES,
+    EDUCACAO_FISICA
+}
---
+++ src/main/java/br/edu/christus/bibliotecapublicavirtual/domain/model/Serie.java
@@ -0,0 +1,7 @@
+package br.edu.christus.bibliotecapublicavirtual.domain.model;
+
+public enum Serie {
+    PRIMEIRO_ANO,
+    SEGUNDO_ANO,
+    TERCEIRO_ANO
+}
